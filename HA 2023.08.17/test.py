d81dbd464d194cf9a94ea94f53a7f45d4360b0d7
ibabyyy <110555899+ibabyyy@users.noreply.github.com>
2023-08-17 14:49:40 +0200

    gdsg

--- HA 2023.08.17/test.py
+++ HA 2023.08.17/test.py
@@ -1,13 +1,13 @@
 import unittest
 from plus import plus1
 
-class Testplus(unittest, TestCase):
+class Testplus(unittest.TestCase):
     def test_plus(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(4, 7), 11)
+        self.assertEqual(plus1(1, 2), 3)
+        self.assertEqual(plus1(4, 7), 11)
 
     def test_plus_floats(self):
-        self.assertAlmostEqual(add(2.5, 5.2), 7.7)
+        self.assertAlmostEqual(plus1(2.5, 5.2), 7.7)
 
 
 if __name__ == "__main__":
